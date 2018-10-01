13bd55850d66c817fae666010e1544a3492ebdad
adodatko <adodatko@dataart.com>
2018-10-01 19:51:39 +0300

    [gui] applied splitter contract in the react controller

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import AdkSplitterForTwo from "./contracts/AdkSplitterForTwo.json";
 import getWeb3 from "./utils/getWeb3";
 import truffleContract from "truffle-contract";
 
@@ -7,13 +7,29 @@ import "./App.css";
 
 class App extends Component
 {
+  // TODO: not using props for hard-coded addresses
+  // since they will move to "state"
+  // when input fields and submit button get added
+
+  // TODO: maybe use nested structures
+  // after getting more familiar with react.js
+
   state =
   {
-      storageValue:    0,
       web3        : null,
       accounts    : null,
-      contract    : null
+      contract    : null,
+      senderAccountAddress        : null,
+      firstReceiverAccountAddress : null,
+      secondReceiverAccountAddress: null,
+      firstReceiverBalance        : null,
+      secondReceiverBalance       : null,
+      senderBalance               : null,
+      contractAddress : null,
+      contractBalance : null
   };
+
+
 
   componentDidMount = async () =>
   {
@@ -26,13 +42,40 @@ class App extends Component
       const accounts = await web3.eth.getAccounts();
 
       // Get the contract instance.
-      const Contract = truffleContract(SimpleStorageContract);
+      const Contract = truffleContract(AdkSplitterForTwo);
       Contract.setProvider(web3.currentProvider);
       const instance = await Contract.deployed();
+      const instanceAddress = await instance.GetContractAddress();
+
+      var sender         = accounts[2];
+      var firstReceiver  = accounts[0];
+      var secondReceiver = accounts[1];
+
+
+      var senderBalance         = await web3.eth.getBalance(sender         );
+      var firstReceiverBalance  = await web3.eth.getBalance(firstReceiver  );
+      var secondReceiverBalance = await web3.eth.getBalance(secondReceiver );
+      var instanceBalance       = await web3.eth.getBalance(instanceAddress);
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState(
+          {
+              web3                        : web3                ,
+              accounts                    : accounts            ,
+              contract                    : instance            ,
+              senderAccountAddress        : sender              ,
+              firstReceiverAccountAddress : firstReceiver       ,
+              secondReceiverAccountAddress: secondReceiver      ,
+              senderBalance               : senderBalance       ,
+              firstReceiverBalance        : firstReceiverBalance,
+              secondReceiverBalance       : secondReceiverBalance,
+              contractAddress             : instanceAddress      ,
+              contractBalance             : instanceBalance
+          },
+
+          this.runExample
+      );
     }
     catch (error)
     {
@@ -48,6 +91,9 @@ class App extends Component
 
   runExample = async () =>
   {
+
+
+/*
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
@@ -58,6 +104,7 @@ class App extends Component
 
     // Update state with the result.
     this.setState({ storageValue: response.toNumber() });
+*/
 
   }; // runExample
 
@@ -74,18 +121,54 @@ class App extends Component
 
 
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 37</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+      <div className="adk Splitter App">
+        <h1>Welcome to the excercise ether splitting app</h1>
+        <p>When the button is tapped, the transfer should happen</p>
+        <h2>Accounts state</h2>
+
+
+        <table>
+        <tbody>
+           <tr>
+               <th></th>
+               <th>Address</th>
+               <th>Balance in Wei</th>
+               <th>Balance in ether</th>
+          </tr>
+
+           <tr>
+               <td>Sender</td>
+               <td>{this.state.senderAccountAddress}</td>
+               <td>{this.state.senderBalance}</td>
+               <td>{ this.state.web3.utils.fromWei( this.state.senderBalance, 'ether') }</td>
+           </tr>
+
+          <tr>
+               <td>First Receiver</td>
+               <td>{this.state.firstReceiverAccountAddress}</td>
+               <td>{this.state.firstReceiverBalance}</td>
+               <td>{ this.state.web3.utils.fromWei( this.state.firstReceiverBalance, 'ether') }</td>
+           </tr>
+
+          <tr>
+               <td>Second Receiver</td>
+               <td>{this.state.secondReceiverAccountAddress}</td>
+               <td>{this.state.secondReceiverBalance}</td>
+               <td>{ this.state.web3.utils.fromWei( this.state.secondReceiverBalance, 'ether') }</td>
+           </tr>
+
+
+
+           <tr>
+               <td>Contract</td>
+               <td>{this.state.contractAddress}</td>
+               <td>{this.state.contractBalance}</td>
+               <td>{ this.state.web3.utils.fromWei( this.state.contractBalance, 'ether') }</td>
+           </tr>
+
+        </tbody>
+        </table>
+
       </div>
     );
   } // render
